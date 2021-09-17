7472c17a0fe71e4077b461401e6330173bceb9a6
Muhammed Kawser Ahmed <55811995+mksaksms@users.noreply.github.com>
2021-09-17 13:21:37 -0400

    Update comm.sh

--- comm.sh
+++ comm.sh
@@ -2,3 +2,9 @@ sudo apt-get install -y gcc git make net-tools libncurses5-dev tftpd zlib1g-dev 
 flex bison libselinux1 gnupg wget diffstat chrpath socat xterm autoconf libtool tar unzip
 texinfo zlib1g-dev gcc-multilib build-essential libsdl1.2-dev libglib2.0-dev zlib1g:i386
 screen pax gzip
+
+
+petalinux-package --boot --format BIN --fsbl ./images/linux/zynq_fsbl.elf --fpga ./images/linux/design_1_wrapper.bit --u-boot --force
+
+
+
