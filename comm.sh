5c6cd7e921422caef9f2d9f6a44f7d9552301a8f
Muhammed Kawser Ahmed <55811995+mksaksms@users.noreply.github.com>
2021-09-17 13:20:18 -0400

    Add files via upload

---
+++ comm.sh
@@ -0,0 +1,4 @@
+sudo apt-get install -y gcc git make net-tools libncurses5-dev tftpd zlib1g-dev libssl-dev
+flex bison libselinux1 gnupg wget diffstat chrpath socat xterm autoconf libtool tar unzip
+texinfo zlib1g-dev gcc-multilib build-essential libsdl1.2-dev libglib2.0-dev zlib1g:i386
+screen pax gzip
