f5a4f40a203bebe5ed88f3ff56b441d1af02601c
Muhammed Kawser Ahmed <55811995+mksaksms@users.noreply.github.com>
2021-09-18 11:16:29 -0400

    Create takepicture.sh

---
+++ takepicture.sh
@@ -0,0 +1,16 @@
+mount /dev/mmcblk0p1 /mnt/
+videotest /dev/video kawser
+cp -f /home/root/kawser.ppm /mnt/
+videotest /dev/video kawser1
+cp -f /home/root/kawser1.ppm /mnt/
+videotest /dev/video kawser2
+cp -f /home/root/kawser2.ppm /mnt/
+
+videotest /dev/video kawser3
+cp -f /home/root/kawser3.ppm /mnt/
+videotest /dev/video kawser4
+cp -f /home/root/kawser4.ppm /mnt/
+videotest /dev/video kawser5
+cp -f /home/root/kawser5.ppm /mnt/
+videotest /dev/video kawser6
+cp -f /home/root/kawser6.ppm /mnt/
